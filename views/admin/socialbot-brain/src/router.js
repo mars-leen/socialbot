d371855d1a43e32f1e3607b0d9f6a05eb6bd6b22
mokepark <858281027@qq.com>
2019-08-18 18:32:02 +0800

    before enter router  intercept login

--- views/admin/socialbot-brain/src/router.js
+++ views/admin/socialbot-brain/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from './layouts/BasicLayout.vue'
+import store from "./store/";
 
 Vue.use(Router);
 
@@ -12,59 +13,103 @@ const routes = [
             {
                 path: '',
                 name: 'default',
-                component: () => import('./views/dashboard/Workplace.vue')
+                component: () => import('./views/dashboard/Workplace.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
             {
                 path: 'workplace',
                 name: 'workplace',
-                component: () => import('./views/dashboard/Workplace.vue')
+                component: () => import('./views/dashboard/Workplace.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
             {
                 path: 'config',
                 name: 'config',
-                component: () => import('./views/config/Config.vue')
+                component: () => import('./views/config/Config.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
             {
                 path: 'config/category',
                 name: 'categoryConfig',
-                component: () => import('./views/config/Category.vue')
+                component: () => import('./views/config/Category.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
             {
                 path: 'config/copywriter',
                 name: 'copywriterConfig',
-                component: () => import('./views/config/Copywriter.vue')
+                component: () => import('./views/config/Copywriter.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
             {
                 path: 'config/tag',
                 name: 'tagConfig',
-                component: () => import('./views/config/Tag.vue')
+                component: () => import('./views/config/Tag.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
             {
                 path: 'config/server',
                 name: 'serverConfig',
-                component: () => import('./views/config/Server.vue')
+                component: () => import('./views/config/Server.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
 
             {
                 path: 'add-commission-product',
                 name: 'addCommissionProduct',
-                component: () => import('./views/media/AddCommissionProduct.vue')
+                component: () => import('./views/media/AddCommissionProduct.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
             {
                 path: 'media/gallery',
                 name: 'gallery',
-                component: () => import('./views/media/Gallery.vue')
+                component: () => import('./views/media/Gallery.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
 
             {
                 path: 'robot/server',
                 name: 'robotServer',
-                component: () => import('./views/robot/Server.vue')
+                component: () => import('./views/robot/Server.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
             {
                 path: 'robot/crawler-item',
                 name: 'crawlerItem',
-                component: () => import('./views/robot/CrawlerItem.vue')
+                component: () => import('./views/robot/CrawlerItem.vue'),
+                meta: {
+                    keepAlive: true,
+                    needLogin:true,
+                }
             },
         ],
     },
@@ -72,9 +117,12 @@ const routes = [
         path: '/dashboard/login',
         name: 'login',
         component:  () => import('./views/user/Login.vue'),
+        meta: {
+            keepAlive: false,
+            needLogin:false,
+        }
     },
 ];
-
 
 const router = new Router({
     mode: 'history',
@@ -82,12 +130,14 @@ const router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
-    /*const title = to.meta && to.meta.title;
-    if (title) {
-      document.title = title
-    }*/
-    next();
+    const token = store.getters.Token();
+    if (to.meta.needLogin && !token) {
+        next("/dashboard/login")
+    }else {
+        next();
+    }
 });
+
 export {
     router
 };
