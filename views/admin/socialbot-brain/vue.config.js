639fc7f97b1471842ea118861b9d63f1ec9ae16a
mokepark <858281027@qq.com>
2019-08-21 12:45:55 +0800

    fix

--- views/admin/socialbot-brain/vue.config.js
+++ views/admin/socialbot-brain/vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 }
             },
             "/storage": {
-                target: "http://149.28.210.141:8081", // 域名
+                target: "http://127.0.0.1:8081", // 域名
                 ws: true, // 是否启用websockets
                 changOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
                 pathRequiresRewrite: {
